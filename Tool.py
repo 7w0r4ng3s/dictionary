2c5e63e2450d4ec53b8e84c86dac70d3c6265bf3
7w0r4ng3s <chr1s_liu@icloud.com>
2018-10-29 22:54:07 -0700

    modify structure

--- Tool.py
+++ Tool.py
@@ -15,7 +15,7 @@ def get_google_sheet(spreadsheet_id, range_name):
     store = file.Storage('credentials.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', scopes)
+        flow = client.flow_from_clientsecrets('json/client_secret.json', scopes)
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
--- word_test.py
+++ word_test.py
@@ -1,5 +1,7 @@
 from Word import Word # the word class including word functions
 from Tool import * # the tool scraping data from Google Sheet
+import random
+from random import sample
 
 gsheet = get_google_sheet(SPREADSHEET_ID, RANGE_NAME)
 df = gsheet2df(gsheet)
@@ -11,3 +13,9 @@ word_list = []
 for num in range(df.shape[0]):
     word_list.append(list(df.iloc[num]))
 
+print('{} words retrieved successfully!'.format(len(word_list)))
+rand_num = random.sample(range(0,61), 61)
+
+# print word_list in random order
+for num in rand_num:
+    print(word_list[num])
