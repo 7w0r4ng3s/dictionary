c4ca92bcfe55d3d612ca30f7f4f522fd8b8f6c4f
7w0r4ng3s <chr1s_liu@icloud.com>
2018-11-06 19:08:21 -0800

    redesign architecture

---
+++ Merge.py
@@ -0,0 +1,51 @@
+from get_sheet import * # the tool scraping data from Google Sheet
+import pandas as pd
+import glob
+import os
+
+id_list = ['101S-DEzHcZYNDw1VZ7NRXD2nkJQfq2TT_zvH9_1zcfU', '19Sk7PWxUS3wkK7WIsyAShRWzk0RNwbS4680e9CIn9C4']
+range_list = ['list_1', 'list_2']
+
+def merge_list():
+	print('Options')
+	print('1 Add Google Sheet \n2 Merge List')
+	user_input = input('Type the number to proceed: ')
+
+	if user_input == '1':
+		# prompt user the number of the target list
+		sheet_id = input('Paste the spreadsheet ID: \n')
+		sheet_range = input('Paste the spreadsheet range name: \n')
+		id_list.append(sheet_id)
+		range_list.append(sheet_range)
+
+	elif user_input == '2':
+		# prompt user the number of the target list
+		choice = int(input('Enter the number of the list you wish to merge into the database: \n'))
+
+		SPREADSHEET_ID = id_list[choice - 1]
+		RANGE_NAME = range_list[choice - 1]
+		print('---------- List Info ----------')
+		print('list_{}'.format(choice))
+		print('Spreadsheet ID:', SPREADSHEET_ID)
+		print('Sheet name:', RANGE_NAME)
+		print()
+		print('---------- Retrieving Word List ----------')
+		print('Waiting...')
+		gsheet = get_google_sheet(SPREADSHEET_ID, RANGE_NAME)
+		df = gsheet2df(gsheet)
+		print('{} words retrieved successfully!'.format(df.shape[0]))
+
+		df.to_csv('csv/list_{}.csv'.format(choice), encoding='utf-8', index=False)
+
+		os.chdir('/Users/7w0r4ng3s/Desktop/Dictionary/csv')
+		results = pd.DataFrame([])
+		    
+		# merging all list by append them to a single dataframe
+		for counter, file in enumerate(glob.glob("list_*")):
+		    namedf = pd.read_csv(file, skiprows=0, usecols=[0,1,2,3])
+		    results = results.append(namedf)
+
+		# convert the dataframe to a new csv file in the same directory
+		# the new csv file containing all words will serve as a database
+		results.reset_index(drop=True).to_csv('/Users/7w0r4ng3s/Desktop/Dictionary/csv/words.csv', index=None)
+
--- WordTool.py
+++ WordTool.py
@@ -2,29 +2,42 @@ import pandas as pd
 
 df = pd.read_csv('csv/words.csv')
 
-def search():
-    word = input('Enter the vocabulary you want to search: ').lower()
+word_list = [] # the dictionary storing all info of individual words
 
-    if word in list(df['word']):
-    	row = df[df['word'] == word]
-    	print()
-    	print(word + '\n')
-    	print()
+for num in range(df.shape[0]):
+    word_list.append(list(df.iloc[num]))
 
-
+words = [l[0] for l in word_list] # a list stroring all words
 
 def search():
-    # TODO: move the search method to WordTool.py
+	# TODO: Add comments
 	word = input('Enter the vocabulary you want to search: ').lower()
 
-	if word in [l[0] for l in rand_list]:
+	if word in words:
 		row = df[df['word'] == word]
-		index = [l[0] for l in rand_list].index(word)
+		index = words.index(word)
 		print()
 		print(word + '\n')
-		print('Knew the word before: {}'.format(rand_list[index][1]))
-		print('Chinese meaning: {}'.format(rand_list[index][2]))
-		print('English meaning: {}'.format(rand_list[index][3]))
-	else:
-		print('Error: {} not in list_1'.format(word))
-		search()
+		print('Knew the word before: {}'.format(word_list[index][1]))
+		print('Chinese meaning: {}'.format(word_list[index][2]))
+		print('English meaning: {}'.format(word_list[index][3]))
+
+	elif word not in words:
+		tmp = []
+		[tmp.append(item) for item in word_list if word.lower() in item[0].lower()]
+		l = [i+1 for i in list(range(len(tmp)))] # index list
+		if len(tmp) == 0:
+			print('Error: {} not in dictionary. Please try again.'.format(word))
+		print()
+		print('Do you mean: ')
+		word_dict = dict(zip(l, tmp))
+		for i in word_dict.keys():
+			print(i, word_dict[i][0])
+
+		print()
+		choice = int(input('Enter the index: '))-1
+		print()
+		print(tmp[choice][0])
+		print('Knew the word before: {}'.format(tmp[choice][1]))
+		print('Chinese meaning: {}'.format(tmp[choice][2]))
+		print('English meaning: {}'.format(tmp[choice][3]))
